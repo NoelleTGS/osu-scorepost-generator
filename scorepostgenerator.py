1f6c861d220432dac94edb9200268f163cf50715
Noelle Zahara <noellezahara@gmail.com>
2024-07-22 04:21:22 -0600

    Initial

---
+++ scorepostgenerator.py
@@ -0,0 +1,156 @@
+from ossapi import Ossapi
+from ossapi.enums import RankStatus
+from rosu_pp_py import Beatmap, Calculator
+import dotenv
+import requests
+import os
+from sys import exit
+import pyperclip as pc
+from circleguard import *
+import webbrowser
+
+dotenv.load_dotenv()
+
+cg = Circleguard(os.getenv("CIRCLEGUARD_API_KEY"))
+
+client_id = os.getenv("OSU_CLIENT_ID")
+client_secret = os.getenv("OSU_CLIENT_SECRET")
+api = Ossapi(client_id, client_secret)
+
+
+def acc_if_fc(score):
+    count300 = score.statistics.count_300
+    count100 = score.statistics.count_100
+    count50 = score.statistics.count_50
+    acc = (300 * count300 + 100 * count100 + 50 * count50) / (300 * (count300 + count100 + count50))
+    return acc * 100
+
+
+def downloadmap(score):
+    print("Downloading beatmap...")
+    url = "https://osu.ppy.sh/osu/" + str(score.beatmap.id)
+    r = requests.get(url)
+    open('osu.osu', 'wb').write(r.content)
+
+
+def calculatePP(function, score, maxcombo):
+    if maxcombo == 0: return 0
+    downloadmap(score)
+    mapfile = Beatmap(path="osu.osu")
+    calc = Calculator(mods=score.mods.value)
+    if function == "curr":
+        print("Calculating score PP...")
+        calc.set_acc(score.accuracy * 100)
+        calc.set_n_misses(score.statistics.count_miss)
+        calc.set_combo(score.max_combo)
+    elif function == "fc":
+        print("Calculating PP if FC...")
+        calc.set_acc(acc_if_fc(score))
+        calc.set_n_misses(0)
+        calc.set_combo(maxcombo)
+    elif function == "ss":
+        print("Calculating PP if SS...")
+        calc.set_acc(100.00)
+        calc.set_n_misses(0)
+        calc.set_combo(maxcombo)
+    else:
+        print("Invalid entry.")
+        quit()
+    perf = calc.performance(mapfile)
+    try:
+        os.remove("osu.osu")
+    except OSError as x:
+        print("Error occurred: %s : %s" % ("osu", x.strerror))
+    return perf.pp
+
+
+gamemode = input("Mode (osu, taiko, fruits, mania): ")
+inputMode = input("User or score: ")
+if inputMode == "user":
+    currentUser = input("Enter a username: ")
+    mode = input("Best or recent: ")
+    try:
+        currentUser = api.user(currentUser)
+    except ValueError:
+        print("User not found.")
+        exit()
+
+    try:
+        score = api.user_scores(currentUser.id, mode, include_fails=True, mode=gamemode, limit=1)[0]
+    except IndexError:
+        print("No scores found.")
+        exit()
+elif inputMode == "score":
+    scoreID = int(input("Enter score ID: "))
+    try:
+        score = api.score(gamemode, scoreID)
+        currentUser = api.user(score.user())
+    except IndexError:
+        print("Score not found.")
+        exit()
+
+beatmap = api.beatmap(beatmap_id=score.beatmap.id)
+star = "%.2f" % round(
+    api.beatmap_attributes(beatmap_id=score.beatmap.id, mods=score.mods, ruleset=gamemode).attributes.star_rating, 2)
+maxcombo = beatmap.max_combo
+
+post = ""
+post += currentUser.username + " | "
+post += score.beatmapset.artist + " - " + score.beatmapset.title + " [" + beatmap.version + "] "
+post += "(" + score.beatmapset.creator + ", " + str(star) + "*) "
+if str(score.mods) != "NM": post += "+" + str(score.mods) + " "
+accuracy = "%.2f" % round(score.accuracy * 100, 2)
+if accuracy == "100.00":
+    post += "SS "
+else:
+    post += "%.2f" % round(score.accuracy * 100, 2) + "% "
+if score.perfect:
+    if accuracy != "100.00":
+        post += "FC "
+else:
+    post += str(score.max_combo) + "/" + str(beatmap.max_combo) + " "
+if score.statistics.count_miss > 0: post += str(score.statistics.count_miss) + "xMiss "
+if not score.passed:
+    post += "FAIL "
+
+leaderboard = api.beatmap_scores(beatmap_id=score.beatmap.id).scores
+for index, item in enumerate(leaderboard):
+    if item.id == score.best_id:
+        post += "#" + str(index + 1) + " "
+        break
+
+if beatmap.status == RankStatus.LOVED: post += "💖 "
+
+post += "| "
+
+if score.pp is None:
+    pp = calculatePP("curr", score, maxcombo)
+else:
+    pp = score.pp
+if beatmap.status == RankStatus.RANKED:
+    if score.passed:
+        post += str(round(pp)) + "pp "
+    else:
+        post += str(round(pp)) + "pp if submitted "
+else:
+    post += str(round(pp)) + "pp if ranked "
+if not score.perfect:
+    post += "(" + str(int(calculatePP("fc", score, maxcombo))) + "pp if FC) "
+
+if score.replay:
+    replay = ReplayMap(score.beatmap.id, currentUser.id)
+    post += "| " + str("%.2f" % round(cg.ur(replay), 2))
+    if "DT" in str(score.mods) or "HT" in str(score.mods):
+        post += " cv. UR "
+    else:
+        post += " UR "
+
+print(post)
+try:
+    pc.copy(post)
+    print("Copied to clipboard.")
+except:
+    print("An error occurred when coping to clipboard.")
+
+if input("Open browser? (y/N) ") == ("y" or "yes"):
+    webbrowser.open_new_tab("https://www.reddit.com/r/osugame/submit")
