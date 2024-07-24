04454ea64e0e3237b08bd3f41b1592b9d639daa4
Noelle Zahara <noellezahara@gmail.com>
2024-07-24 15:33:42 -0600

    Only ask to consider fails if retrieving recent score

--- scorepostgenerator.py
+++ scorepostgenerator.py
@@ -75,9 +75,12 @@ if inputMode == "user":
         print("User not found.")
         exit()
     mode = input("Best or recent: ")
-    fails = input("Consider fails? (Y/n) ")
-    if fails == "Y" or fails == "y" or fails == "": fails = True
-    if fails == "N" or fails == "n": fails = False
+    if mode == "recent":
+        fails = input("Consider fails? (Y/n) ")
+        if fails == "Y" or fails == "y" or fails == "": fails = True
+        if fails == "N" or fails == "n": fails = False
+    else:
+        fails = True
 
     try:
         score = api.user_scores(currentUser.id, mode, include_fails=fails, mode=gamemode, limit=1)[0]
