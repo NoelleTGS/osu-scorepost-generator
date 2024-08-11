5f115a00dda47a998cd9c883ceec50b132d09e60
Noelle Zahara <noellezahara@gmail.com>
2024-08-11 04:02:25 -0600

    Use osu.py for accuracy retrieval
    Closes #4

--- scorepostgenerator.py
+++ scorepostgenerator.py
@@ -103,11 +103,11 @@ for mod in score_osupy.mods:
 if score.max_combo > (beatmap.max_combo * 0.99):
     score.perfect = True
 
-accuracy = "%.2f" % round(score.accuracy * 100, 2)
+accuracy = "%.2f" % round(score_osupy.accuracy * 100, 2)
 if accuracy == "100.00":
     post += "SS "
 else:
-    post += "%.2f" % round(score.accuracy * 100, 2) + "% "
+    post += "%.2f" % round(score_osupy.accuracy * 100, 2) + "% "
 if score.perfect:
     if accuracy != "100.00":
         post += "FC "
